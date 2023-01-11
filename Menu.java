972b0a00156e38d9d7477c0715174f20e8aef427
William-Trudeau <349468900@gapps.yrdsb.ca>
2023-01-11 11:56:54 -0500

    Menu Background

--- Menu.java
+++ Menu.java
@@ -16,7 +16,7 @@ public class Menu extends World
     public Menu()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(850, 450, 1); 
+        super(800, 550, 1); 
         StartButton start = new StartButton();
         if (Greenfoot.mouseClicked(start))
         {
