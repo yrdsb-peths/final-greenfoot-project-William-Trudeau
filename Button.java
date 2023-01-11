0ca0ca04bd81deae26af4a45532cc7175d511a62
William-Trudeau <349468900@gapps.yrdsb.ca>
2023-01-11 13:25:33 -0500

    Button Class

--- Button.java
+++ Button.java
@@ -1,19 +1,30 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class StartButton here.
+ * The buttons class to 
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class StartButton extends Actor
+public class Button extends Actor
 {
+    private String name;
+    
+    
+    
+    public Button(String name) {
+        this.name = name;
+    }
+    
     /**
      * Act - do whatever the StartButton wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        
-    }
+        if (Greenfoot.mouseClicked(this) && this.name == "StartButton") {
+            Greenfoot.setWorld(new Levels());
+        }
+    }    
 }
+       
--- Menu.java
+++ Menu.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Menu here.
+ * GameStart Menu
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author William Wang
+ * @version 1.0.0
  */
 public class Menu extends World
 {
@@ -17,10 +17,7 @@ public class Menu extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 550, 1); 
-        StartButton start = new StartButton();
-        if (Greenfoot.mouseClicked(start))
-        {
-            System.out.println("Clicked");
-        }
+        Button start = new Button("StartButton");
+        addObject(start, 200, 200);
     }
 }
