7b07a35dae9be4574042476eb5a4c06b2b9a6d61
William-Trudeau <349468900@gapps.yrdsb.ca>
2023-01-13 12:10:23 -0500

    Shop Button

--- Menu.java
+++ Menu.java
@@ -17,10 +17,16 @@ public class Menu extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 550, 1); 
         Button logo = new Button("Logo");
-        addObject(logo, 400, 85);
+        addObject(logo, 400, 90);
         logo.setImage("logo.gif");
         Button start = new Button("StartButton");
-        addObject(start, 400, 220);
+        addObject(start, 400, 225);
         start.setImage("button_start.png"); 
+        Button guide = new Button("Guide");
+        addObject(guide, 400, 315);
+        guide.setImage("button_guide.png");
+        Button shop = new Button("Shop");
+        addObject(shop, 400, 405);
+        shop.setImage("button_shop.png");
     }
 }
